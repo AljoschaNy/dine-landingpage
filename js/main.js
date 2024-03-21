0db6dfbcb8b8784263f7b620a75ec94087b72161
Aljoscha Nyang <a.nyang@gmx.de>
2024-03-21 19:14:12 +0100

    Dynamically update information based on the selected event and smooth out transitions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
+import getEventInfos from "./utils/events.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   const eventItems = document.querySelectorAll(".event-list__item");
   const eventImage = document.querySelector(".event-image");
+  const eventHeading = document.querySelector(".event-info__header");
+  const eventdescription = document.querySelector(".event-info__text");
 
   eventItems.forEach((item) => {
     item.addEventListener("mouseover", function () {
-      eventImage.src = this.getAttribute("data-image");
-      eventImage.alt = this.getAttribute("data-image-alt");
+      const event = this.getAttribute("data-image-package");
+      const infos = getEventInfos(event);
+
+      eventImage.style.opacity = 0;
+      eventHeading.style.opacity = 0;
+      eventdescription.style.opacity = 0;
+
+      setTimeout(() => {
+        eventImage.src = infos.image.desktopSrc;
+        eventImage.alt = infos.image.alt;
+        eventHeading.innerText = infos.heading;
+        eventdescription.innerText = infos.description;
+
+        eventImage.style.opacity = 1;
+        eventHeading.style.opacity = 1;
+        eventdescription.style.opacity = 1;
+      }, 400);
 
       eventItems.forEach((i) => i.classList.remove("active"));
       this.classList.add("active");
