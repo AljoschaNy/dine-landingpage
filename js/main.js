5dd7368cb0c30d25fcff24c7d2793f419f6313f0
Aljoscha Nyang <a.nyang@gmx.de>
2024-03-21 15:02:58 +0100

    Change event image src and alt text based on selected event

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const eventItems = document.querySelectorAll(".event-list__item");
+  const eventImage = document.querySelector(".event-image");
+
+  eventItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      eventImage.src = this.getAttribute("data-image");
+      eventImage.alt = this.getAttribute("data-image-alt");
+
+      eventItems.forEach((i) => i.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+});
