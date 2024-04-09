829f3b69d6a7af2110b6dbe187555a34fe40b2aa
Aljoscha Nyang <a.nyang@gmx.de>
2024-04-09 23:24:15 +0200

    Refactor code in peopleCount.js to remove redundant lines

--- js/utils/peopleCount.js
+++ js/utils/peopleCount.js
@@ -3,20 +3,22 @@ import { formInputs } from "./formElements.js";
 const addPersonBtn = document.getElementById("btn-add-people");
 const removePersonBtn = document.getElementById("btn-reduce-people");
 let peopleCountElem = formInputs.peopleCountElem;
-let peopleCountInt = parseInt(formInputs.peopleCountElem.innerText);
+
+const updatePeopleCount = (change) => {
+  let peopleCount = parseInt(formInputs.peopleCountElem.innerText);
+  peopleCount += change;
+  peopleCount = Math.min(Math.max(peopleCount, 1), 20);
+  peopleCountElem.innerText = peopleCount;
+  document.getElementById("people-count-text").innerText =
+    peopleCount > 1 ? "people" : "person";
+};
 
 const addPerson = () => {
-  if (peopleCountInt < 20) peopleCountInt++;
-  peopleCountElem.innerText = peopleCountInt;
-  document.getElementById("people-count-text").innerText =
-    peopleCountInt > 1 ? "people" : "person";
+  updatePeopleCount(1);
 };
 
 const removePerson = () => {
-  if (peopleCountInt > 1) peopleCountInt--;
-  peopleCountElem.innerText = peopleCountInt;
-  document.getElementById("people-count-text").innerText =
-    peopleCountInt > 1 ? "people" : "person";
+  updatePeopleCount(-1);
 };
 
 const handlePeopleCountChange = () => {
